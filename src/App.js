a2607784a883ca3271d0ac0b610d1e94d6a70afd
Olu <theoafactor@gmail.com>
2022-05-04 12:55:42 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
+import React from "react"
 import logo from './logo.svg';
 import './App.css';
+import Books from './Components/Books/Books';
+import Themer from './Components/Themer/Themer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(){
+    super()
+
+    this.state = {
+      backgroundColor: '#272C33'
+    }
+
+
+
+
+  }
+
+  changeTheme = (theme) => {
+    if(theme == "dark"){
+      this.setState({
+        backgroundColor: 'black'
+      })
+    }
+
+    if(theme == "danger"){
+      this.setState({
+        backgroundColor: 'red'
+      })
+    }
+  }
+  // changeTheme(theme){
+  
+    
+
+  //   // if(theme == "danger"){
+  //   //   this.setState({
+  //   //     backgroundColor: 'red'
+  //   //   })
+  //   // }
+
+  // }
+
+  render(){
+    return (
+      <div className="App" style={{backgroundColor: this.state.backgroundColor, color: 'white'}}>
+  
+        <header className="App-header">
+            <Themer changeTheme={this.changeTheme}/>
+            <Books />
+        </header>
+      
+      </div>
+    );
+  }
+
+
 }
 
-export default App;
+export default App
